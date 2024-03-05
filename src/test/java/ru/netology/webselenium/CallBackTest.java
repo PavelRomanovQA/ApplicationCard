5d303d3ad64998c2dd4c679314765d181f880743
Pavel <pavel.0gu@.com>
2024-03-05 13:25:19 +0700

    Fixed commit

--- src/test/java/ru/netology/webselenium/CallBackTest.java
+++ src/test/java/ru/netology/webselenium/CallBackTest.java
@@ -27,27 +27,59 @@ public class CallBackTest {
     }
 
     @Test
-    void shouldntPostEmptyName() {
+    void nameIncorrect() {
         open("http://localhost:9999");
         SelenideElement form = $("form");
-        form.$("[data-test-id=name] input").setValue(" ");
-        form.$("[data-test-id=phone] input").setValue("+798008585555");
+        form.$("[data-test-id=name] input").setValue("Ивановичёв Иван");
+        form.$("[data-test-id=phone] input").setValue("+79808585555");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         form.$("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText
-                ("Поле обязательно для заполнения"));
+                ("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
 
     @Test
-    void shouldntPostEmptyPhone() {
-        open("http://localhost:9999");
+    void nameIncorrect2() {
+        open ("http://localhost:9999");
         SelenideElement form = $("form");
-        form.$("[data-test-id=name] input").setValue("Иваныч-Ивановичев Иван");
-        form.$("[data-test-id=phone] input").setValue("");
+        form.$("[data-test-id=name] input")
+                .setValue("Ivan Ivanovich");
+        form.$("[data-test-id=phone] input").setValue("+79808585555");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        form.$("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText
-                ("Поле обязательно для заполнения"));
+        form.$("[data-test-id=name].input_invalid .input__sub")
+                .shouldHave(exactText
+                        ("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+
+    @Test
+    void incorrectPhoneNumber() {
+        open ("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input")
+                .setValue("Иваныч-Ивановичев Иван");
+        form.$("[data-test-id=phone] input").setValue("79808585555");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        form.$("[data-test-id=phone].input_invalid .input__sub")
+                .shouldHave(exactText
+                        ("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+
+
+    @Test
+    void agreementNotMarked(){
+        open ("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input")
+                .setValue("Иваныч-Ивановичев Иван");
+        form.$("[data-test-id=phone] input").setValue("+79808585555");
+        form.$(".button").click();
+        form.$("[data-test-id=agreement].input_invalid")
+                .shouldHave(visible);
+
     }
 }
